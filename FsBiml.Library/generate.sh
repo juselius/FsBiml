477e5cfd75716d4e3198c2d07f1d5fe7a3b20359
Jonas Juselius <jonas.juselius@itpartner.no>
2016-10-28 11:57:52 +0200

    Fix regex and update generated bindings.

--- FsBiml.Library/generate.sh
+++ FsBiml.Library/generate.sh
@@ -2,8 +2,8 @@
 
 [ ! -f biml.xsd ] && wget "http://bimlscript.com/Content/Docs/biml.xsd"
 
-elems=`sed -n 's/^.*<xs:element .* name="\([^"]\+\)" .*/\1/p' biml.xsd |sort |uniq`
-attrs=`sed -n 's/^.*<xs:attribute .* name="\([^"]\+\)" .*/\1/p' biml.xsd |sort |uniq`
+elems=`sed -n 's/^.*<xs:element\( name=\| .* name=\)"\([^"]\+\)" .*/\2/p' biml.xsd |sort |uniq`
+attrs=`sed -n 's/^.*<xs:attribute\( name=\| .* name=\)"\([^"]\+\)" .*/\2/p' biml.xsd |sort |uniq`
 
 preamble () {
     cat << EOF > $1.fs
@@ -22,9 +22,7 @@ indent () {
 elt () {
     preamble $2
     indent $2
-    echo "let Biml = eltAttr \"Biml\" [xmlns \"http://schemas.varigence.com/biml.xsd\"]" >> $2.fs
-    indent $2
-    echo "let BimlAttr = eltAttr \"Biml\"" >> $2.fs
+    echo "let BIML = eltAttr \"Biml\" [xmlns \"http://schemas.varigence.com/biml.xsd\"]" >> $2.fs
     echo ""  >> $2.fs
     for i in $1; do
         indent $2
